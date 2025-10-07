2dedbeadccd7b77d10de523d86f9e29f8dbde239
Matthias Arnold <42539387+DrMatthiasArnold@users.noreply.github.com>
2025-10-07 06:00:47 +0200

    Replaced pip by pip3

--- dev/build_and_install.sh
+++ dev/build_and_install.sh
@@ -9,14 +9,14 @@ cd "$ROOT_DIR/../lads_opcua_client"
 
 # Build and install package
 python3 -m build
-pip install dist/lads_opcua_client-0.0.1.tar.gz
+pip3 install dist/lads_opcua_client-0.0.1.tar.gz
 
 # Navigate to lads_opcua_viewer
 cd "$ROOT_DIR/../lads_opcua_viewer"
 
 # Build and install package
 python3 -m build
-pip install dist/lads_opcua_viewer-0.0.1.tar.gz
+pip3 install dist/lads_opcua_viewer-0.0.1.tar.gz
 
 # Return to project root
 echo -e "
